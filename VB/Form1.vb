3142def97b649117dd50ba9911300e30c127fe3c
DevExpressExampleBot <fakeEmail>
2023-09-22 15:22:35 +0400

    Source auto update [skip ci]

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,35 +1,30 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
-Imports DevExpress.XtraGrid.Views.Grid
-Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
-Imports DevExpress.Utils
 
 Namespace WindowsApplication1
-	Partial Public Class Form1
-		Inherits Form
-				Private Function CreateTable(ByVal RowCount As Integer) As DataTable
-			Dim tbl As New DataTable()
-			tbl.Columns.Add("Name", GetType(String))
-			tbl.Columns.Add("ID", GetType(Integer))
-			tbl.Columns.Add("Number", GetType(Integer))
-			tbl.Columns.Add("Date", GetType(DateTime))
-			For i As Integer = 0 To RowCount - 1
-				tbl.Rows.Add(New Object() { String.Format("Name{0}", i), i, 3 - i, DateTime.Now.AddDays(i) })
-			Next i
-			Return tbl
-				End Function
 
+    Public Partial Class Form1
+        Inherits Form
 
-		Public Sub New()
-			InitializeComponent()
-			gridControl1.DataSource = CreateTable(20)
-			Dim TempMultiSelectionHelper As MultiSelectionHelper = New MultiSelectionHelper(gridView1)
-		End Sub
-	End Class
+        Private Function CreateTable(ByVal RowCount As Integer) As DataTable
+            Dim tbl As DataTable = New DataTable()
+            tbl.Columns.Add("Name", GetType(String))
+            tbl.Columns.Add("ID", GetType(Integer))
+            tbl.Columns.Add("Number", GetType(Integer))
+            tbl.Columns.Add("Date", GetType(Date))
+            For i As Integer = 0 To RowCount - 1
+                tbl.Rows.Add(New Object() {String.Format("Name{0}", i), i, 3 - i, Date.Now.AddDays(i)})
+            Next
+
+            Return tbl
+        End Function
+
+        Public Sub New()
+            InitializeComponent()
+            gridControl1.DataSource = CreateTable(20)
+            Dim TempMultiSelectionHelper As MultiSelectionHelper = New MultiSelectionHelper(gridView1)
+        End Sub
+    End Class
 End Namespace
--- VB/MultiSelectionHelper.vb
+++ VB/MultiSelectionHelper.vb
@@ -1,48 +1,47 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
 Imports System.ComponentModel
-Imports System.Data
 Imports System.Drawing
-Imports System.Text
 Imports System.Windows.Forms
 Imports DevExpress.XtraGrid.Views.Grid
 Imports DevExpress.XtraGrid.Views.Grid.ViewInfo
 Imports DevExpress.Utils
 
 Namespace WindowsApplication1
-	Public Class MultiSelectionHelper
 
-		Private _GridView As GridView
-		Public Sub New(ByVal gridView As GridView)
-			_GridView = gridView
-			InitProperties()
-			SubscribeEvents()
-		End Sub
-		Private Sub InitProperties()
-			_GridView.OptionsBehavior.Editable = False
-			_GridView.OptionsSelection.MultiSelect = True
-			_GridView.OptionsSelection.EnableAppearanceFocusedCell = False
-			_GridView.FocusRectStyle = DrawFocusRectStyle.None
-		End Sub
-		Private Sub SubscribeEvents()
-			AddHandler _GridView.MouseDown, AddressOf _GridView_MouseDown
-		End Sub
+    Public Class MultiSelectionHelper
 
-		Private Sub _GridView_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs)
-			OnMouseDown(e)
-		End Sub
-		Private Sub OnMouseDown(ByVal e As MouseEventArgs)
-			Dim hi As GridHitInfo = _GridView.CalcHitInfo(e.Location)
-			If (Not hi.InRow) Then
-				Return
-			End If
-			_GridView.FocusedRowHandle = hi.RowHandle
-			_GridView.FocusedColumn = hi.Column
-			_GridView.InvertRowSelection(hi.RowHandle)
-			DXMouseEventArgs.GetMouseArgs(e).Handled = True
+        Private _GridView As GridView
 
-		End Sub
+        Public Sub New(ByVal gridView As GridView)
+            _GridView = gridView
+            InitProperties()
+            SubscribeEvents()
+        End Sub
 
-	End Class
+        Private Sub InitProperties()
+            _GridView.OptionsBehavior.Editable = False
+            _GridView.OptionsSelection.MultiSelect = True
+            _GridView.OptionsSelection.EnableAppearanceFocusedCell = False
+            _GridView.FocusRectStyle = DrawFocusRectStyle.None
+        End Sub
+
+        Private Sub SubscribeEvents()
+            AddHandler _GridView.MouseDown, AddressOf _GridView_MouseDown
+        End Sub
+
+        Private Sub _GridView_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs)
+            OnMouseDown(e)
+        End Sub
+
+        Private Sub OnMouseDown(ByVal e As MouseEventArgs)
+            Dim hi As GridHitInfo = _GridView.CalcHitInfo(e.Location)
+            If Not hi.InRow Then
+                Return
+            End If
+
+            _GridView.FocusedRowHandle = hi.RowHandle
+            _GridView.FocusedColumn = hi.Column
+            _GridView.InvertRowSelection(hi.RowHandle)
+            DXMouseEventArgs.GetMouseArgs(e).Handled = True
+        End Sub
+    End Class
 End Namespace
